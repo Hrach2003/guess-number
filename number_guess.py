d94464302052f5e6345882c93ad909c90da50068
Hrach2003 <155984992+Hrach2003@users.noreply.github.com>
2024-01-10 13:34:52 +0400

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,15 @@
+# import random
+# secret_number = random.randint(1, 10)
+# guess = 0
+# # while secret_number:
+# #     guess = int(input("Enter the number: "))
+# #     if guess == secret_number:
+# #         print(f"{guess} = {secret_number}")
+# #         break
+# #     else:
+# #         print("Havasar chen")
+# while guess != secret_number:
+#     guess = int(input("Enter the number: "))
+#     if guess != secret_number:
+#         print("Try again!")
+# print("You win")
